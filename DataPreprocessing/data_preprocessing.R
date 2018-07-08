573c36516b9f1828168de2153e42d751ed4751cf
Muhammad Affan Khan <maffan@folio3.com>
2018-07-08 19:07:45 +0500

    split data-set in training and testing

--- DataPreprocessing/data_preprocessing.R
+++ DataPreprocessing/data_preprocessing.R
@@ -18,3 +18,11 @@ dataset$Country = factor(dataset$Country,
 dataset$Purchased = factor(dataset$Purchased,
                          levels = c('Yes','No'),
                          labels = c(1,0))
+
+# Splitting the dataset into training set and test set.
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased,SplitRatio = 0.8)
+training_set = subset(dataset,split == TRUE)
+test_set = subset(dataset,split == FALSE)
